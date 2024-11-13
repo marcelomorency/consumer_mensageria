b9553bfb2b51f57cca1df713d7bf5045fc77712b
Marcelo Mont-Morency Gondim Rodrigues <morencymarcelo@gmail.com>
2024-11-13 14:51:28 -0300

    valor recebido menos subtração por 20

--- consumer.js
+++ consumer.js
@@ -1,4 +1,4 @@
-const dotenv = require("dotenv").config();
+/*const dotenv = require("dotenv").config();
 
 const amqp = require("amqplib");
 
@@ -29,4 +29,42 @@ async function receiveMessage() {
     console.error(error);
   }
 }
+receiveMessage();
+*/
+
+const amqp = require("amqplib");
+const dotenv = require("dotenv").config();
+
+let api_key = process.env.AMQP;
+
+async function receiveMessage() {
+  let queue = "hello";
+
+  try {
+    const connection = await amqp.connect(api_key);
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(queue, { durable: false });
+    console.log("Aguardando a fila: " + queue);
+
+    channel.consume(
+      queue,
+      (msg) => {
+        if (msg !== null) {
+          let receivedData = JSON.parse(msg.content.toString());
+          let currentValue = receivedData.value;
+
+          // Subtrai um valor do valor recebido
+          let newValue = currentValue - 20.0;
+          console.log(`Valor recebido: ${currentValue}`);
+          console.log(`Valor recebido após subtração: ${newValue}`);
+        }
+      },
+      { noAck: true }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 receiveMessage();
